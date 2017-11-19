f7de89684f6f24f735b152204737818315a946d3
Sathaporn Sunthornpan <s57122201043@ssru.ac.th>
2017-11-19 14:11:36 +0700

    init demoMVCWeb : Web Application (ASP.NET Framework)

---
+++ DemoMVCWeb/DemoMVCWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoMVCWeb.Startup))]
+namespace DemoMVCWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
