10c3313986faf2e3a77e339e9667742783780be0
Sathaporn Sunthornpan <s57122201043@ssru.ac.th>
2017-11-19 16:21:34 +0700

    init Web Api (ASP.NET Web Application )

---
+++ DemoApiEmployee/DemoApiEmployee/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DemoApiEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DemoApiEmployee/DemoApiEmployee/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DemoApiEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
